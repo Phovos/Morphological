4e28501118566d61e0ed0113c4aae23e1869ab35
Phovos <datadeve@outlook.com>
2025-03-01 06:21:48 +0000

    staging branch init with new main.py everything old is in dev atm

--- source/external/emain.py
+++
@@ -1,4 +0,0 @@
-# !/usr/bin/env python3
-# /src/external/emain.py - ExternalMain source code
-# located here to enable the ExternalMain to include external libraries
-# simply validated and passed to pdm
