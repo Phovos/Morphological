90a418fb4f4f3c27621b348fb87b6602fb9e35f5
Phovos <datadeve@outlook.com>
2025-07-01 11:04:46 -0500

    Update provisioner.sh

--- provisioner.sh
+++ provisioner.sh
@@ -2,4 +2,6 @@ echo "MY_VAR='Hello, world!'" > .env
 
 uv run --env-file .env -- python -c 'import os; print(os.getenv("MY_VAR"))'
 
+# https://docs.astral.sh/uv/guides/integration/jupyter/#using-jupyter-within-a-project
 # uv run --with jupyter jupyter lab
+# uv run ipython kernel install --user --env VIRTUAL_ENV $(pwd)/.venv --name=project
